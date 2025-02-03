d24d5c20c56c890fa3c936d60f2a10aa65190471
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-03 11:13:44 +0700

    feat: Create balanceapp with models, admin, views, and initial tests

---
+++ balanceapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Balance(models.Model):
+    date = models.DateField(unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    income = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    expense = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    kertas = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    receh = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    ewallet = models.DecimalField(max_digits=10, decimal_places=2, default=0)
