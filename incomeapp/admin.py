f62553f7cf7caa6d900a5c59653990a0da71f1c5
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-03 20:18:38 +0700

    feat: Refactor Income model to use ForeignKey for location and payment fields; update admin form and migrations

--- incomeapp/admin.py
+++ incomeapp/admin.py
@@ -9,10 +9,27 @@ class IncomeAdminForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'purpose_details': FilteredSelectMultiple('Purpose Details', is_stacked=False),
+            'related_parties': FilteredSelectMultiple('Related Parties', is_stacked=False),
         }
 
 @admin.register(Income)
 class IncomeAdmin(admin.ModelAdmin):
     form = IncomeAdminForm
-    list_display = ('date_time', 'purpose', 'amount', 'related_party', 'location')  # Updated field name
-    search_fields = ('purpose__name', 'related_party', 'location')
+    list_display = ('date_time', 'purpose', 'amount', 'location')
+    search_fields = ('purpose__name', 'location__location_name')
+
+    # Define fieldsets to create sections in the admin form
+    fieldsets = (
+        ('Common Information', {
+            'fields': ('date_time', 'purpose', 'purpose_details')
+        }),
+        ('Financial Information', {
+            'fields': ('amount', 'payment_method', 'payment_status')
+        }),
+        ('Location and Related Parties', {
+            'fields': ('location', 'related_parties')
+        }),
+        ('Additional Information', {
+            'fields': ('notes',)
+        }),
+    )
--- incomeapp/models.py
+++ incomeapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from purposeapp.models import Purpose, PurposeDetail
 from balanceapp.models import Balance
+from locationapp.models import Location, RelatedParty
+from paymentapp.models import PaymentMethod, PaymentStatus
 from django.utils import timezone
 
 class Income(models.Model):
@@ -9,10 +11,10 @@ class Income(models.Model):
     purpose_details = models.ManyToManyField(PurposeDetail, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     notes = models.TextField(blank=True)
-    related_party = models.CharField(max_length=100, blank=True)
-    location = models.CharField(max_length=100, blank=True)
-    payment_method = models.CharField(max_length=50, blank=True)
-    status = models.CharField(max_length=50, blank=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)  # Reference Location
+    related_parties = models.ManyToManyField(RelatedParty, blank=True)  # Updated to use ManyToManyField
+    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.SET_NULL, null=True, blank=True)
+    payment_status = models.ForeignKey(PaymentStatus, on_delete=models.SET_NULL, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         super(Income, self).save(*args, **kwargs)
