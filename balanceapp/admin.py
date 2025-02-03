32e3009f2ef78e9c5212a7df9664e74ce014d1a9
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-03 20:16:59 +0700

    feat: Add ordering by date_time to BalanceAdmin in admin panel

--- balanceapp/admin.py
+++ balanceapp/admin.py
@@ -3,4 +3,5 @@ from .models import Balance
 
 @admin.register(Balance)
 class BalanceAdmin(admin.ModelAdmin):
-    list_display = ('date_time', 'balance', 'income', 'expense')  # Updated field name
+    list_display = ('date_time', 'balance', 'income', 'expense')
+    ordering = ('date_time',)
