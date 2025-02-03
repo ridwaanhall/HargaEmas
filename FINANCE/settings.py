706477c118c16371ed61578ba866e4ec092b6c70
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-03 11:45:53 +0700

    feat: Add incomeapp, expenseapp, and balanceapp to installed apps in settings

--- FINANCE/settings.py
+++ FINANCE/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'incomeapp',
+    'expenseapp',
+    'balanceapp',
 ]
 
 MIDDLEWARE = [
