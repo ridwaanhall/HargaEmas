d52f6e643dd8b069573256dcf4d4c917a86a2a24
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-05 17:56:47 +0700

    chore: update database configuration formatting

--- FINANCE/settings.py
+++ FINANCE/settings.py
@@ -95,18 +95,18 @@ WSGI_APPLICATION = 'FINANCE.wsgi.application'
 
 if config('CONFIG', default=False, cast=bool):
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('PGDATABASE'),
-        'USER': config('PGUSER'),
-        'PASSWORD': config('PGPASSWORD'),
-        'HOST': config('PGHOST'),
-        'PORT': config('PGPORT', 5432),
-        'OPTIONS': {
-        'sslmode': 'require',
-        },
-        'DISABLE_SERVER_SIDE_CURSORS': True,
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('PGDATABASE'),
+            'USER': config('PGUSER'),
+            'PASSWORD': config('PGPASSWORD'),
+            'HOST': config('PGHOST'),
+            'PORT': config('PGPORT', 5432),
+            'OPTIONS': {
+                'sslmode': 'require',
+            },
+            'DISABLE_SERVER_SIDE_CURSORS': True,
+        }
     }
 else:
     DATABASES = {
