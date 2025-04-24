0f65cee47d600357f4eed6b0f13e020251c7c305
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-04-25 00:41:01 +0700

    fix: add DATA_URL configuration to settings for improved data management

--- FINANCE/settings.py
+++ FINANCE/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+DATA_URL = config('DATA_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
